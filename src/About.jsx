90773a6b8a88d2af4d241864260f28e9dbdf1577
n4ilvh <nailikespie@gmail.com>
2025-12-24 15:20:21 -0500

    update about

--- src/About.jsx
+++ src/About.jsx
@@ -3,23 +3,25 @@ function About() {
   return (
     <div className="about">
       <div className="about-container">
-        <img src={Image} alt="description" className="about-img"/>
+        <div className="about-image-container">
+          <img src={Image} alt="description" className="about-img"/>
+        </div>
         <div className="about-info">
           <div className="about-title">About Me</div>
-          <div>location: "Toronto, Canada"</div>
-          <div>born: 2006</div>
-          <div>interests: ["gaming", "music", "mechanical keyboards", "electric guitar", "basketball"]</div>
-          <div>school: "Carleton University"</div>
-          <div>skills: &#123;</div>
-          <div>&nbsp;&nbsp;languages: ["Java", "Python", "C", "HTML, CSS"],</div>
-          <div>&nbsp;&nbsp;frameworks: ["React", "Three.js"],</div>
-          <div>&nbsp;&nbsp;tools: ["Git", "Vite"],</div>
-          <div>&nbsp;&nbsp;focus: ["Creative Coding", "Interactive Systems"]</div>
-          <div>&#125;</div>
+          <div className="about-text">
+            <div>location: "Toronto, Canada"</div>
+            <div>born: 2006</div>
+            <div>interests: ["gaming", "music", "mechanical keyboards", "electric guitar", "basketball"]</div>
+            <div>school: "Carleton University"</div>
+            <div>skills: &#123;</div>
+            <div>&nbsp;&nbsp;languages: ["Java", "Python", "C", "HTML, CSS"],</div>
+            <div>&nbsp;&nbsp;frameworks: ["React", "Three.js"],</div>
+            <div>&nbsp;&nbsp;tools: ["Git", "Vite"],</div>
+            <div>&nbsp;&nbsp;focus: ["Creative Coding", "Interactive Systems"]</div>
+            <div>&#125;</div>
+          </div>
         </div>
       </div>
-      
-      
     </div>
 
     // skills 
