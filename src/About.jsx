2de667a8c8c3ca023279ea21ddabe44645fc5f10
n4ilvh <nailikespie@gmail.com>
2025-12-24 14:58:55 -0500

    update about me

--- src/About.jsx
+++ src/About.jsx
@@ -1,8 +1,28 @@
+import Image from "../media/selfie.jpg";
 function About() {
   return (
     <div className="about">
-      <h1>About</h1>
+      <div className="about-container">
+        <img src={Image} alt="description" className="about-img"/>
+        <div className="about-info">
+          <div className="about-title">About Me</div>
+          <div>location: "Toronto, Canada"</div>
+          <div>born: 2006</div>
+          <div>interests: ["gaming", "music", "mechanical keyboards", "electric guitar", "basketball"]</div>
+          <div>school: "Carleton University"</div>
+          <div>skills: &#123;</div>
+          <div>&nbsp;&nbsp;languages: ["Java", "Python", "C", "HTML, CSS"],</div>
+          <div>&nbsp;&nbsp;frameworks: ["React", "Three.js"],</div>
+          <div>&nbsp;&nbsp;tools: ["Git", "Vite"],</div>
+          <div>&nbsp;&nbsp;focus: ["Creative Coding", "Interactive Systems"]</div>
+          <div>&#125;</div>
+        </div>
+      </div>
+      
+      
     </div>
+
+    // skills 
   );
 }
 
