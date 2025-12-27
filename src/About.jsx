d2d42b6c47121670c6cd252536e7d0285dc88475
n4ilvh <nailikespie@gmail.com>
2025-12-27 17:19:31 -0500

    update

--- src/About.jsx
+++ src/About.jsx
@@ -1,25 +1,26 @@
 import Image from "../media/selfie.jpg";
 function About() {
   return (
-    <div className="about">
+    <div className="about"> 
+      <div className="about-title">About</div>
       <div className="about-container">
-        <div className="about-image-container">
+        {/* <div className="about-image-container">
           <img src={Image} alt="description" className="about-img"/>
         </div>
         <div className="about-info">
-          <div className="about-title">About Me</div>
           <div className="about-text">
             <div>location: "Toronto, Canada"</div>
             <div>born: 2006</div>
-            <div>interests: ["gaming", "music", "mechanical keyboards", "electric guitar", "basketball"]</div>
             <div>school: "Carleton University"</div>
+            <div>interests: ["gaming", "music", "mechanical keyboards", "electric guitar", "basketball"]</div>
             <div>skills: &#123;</div>
             <div>&nbsp;&nbsp;languages: ["Java", "Python", "C", "HTML, CSS"],</div>
             <div>&nbsp;&nbsp;frameworks: ["React", "Three.js"],</div>
             <div>&nbsp;&nbsp;tools: ["Git", "Vite"],</div>
             <div>&#125;</div>
+            I like computers, clicky keyboards and creatings things that mean something
           </div>
-        </div>
+        </div> */}
       </div>
     </div>
 
