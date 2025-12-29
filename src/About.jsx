3e615da3bf97c93f46c29ea399ddab3bb22503cc
n4ilvh <nailikespie@gmail.com>
2025-12-28 19:39:59 -0500

    update

--- src/About.jsx
+++ src/About.jsx
@@ -1,12 +1,16 @@
 import Image from "../media/selfie.jpg";
+import Barcode from "../media/barcode.webp";
 function About() {
   return (
     <div className="about"> 
-      <div className="about-title">About</div>
+    <div className="about-image-container">
+          <img src={Barcode} alt="description" className="about-img"/>
+        </div>
+      {/* <div className="about-title">About</div> */}
       <div className="about-container">
         {/* <div className="about-image-container">
           <img src={Image} alt="description" className="about-img"/>
-        </div>
+        </div> */}
         <div className="about-info">
           <div className="about-text">
             <div>location: "Toronto, Canada"</div>
@@ -18,9 +22,9 @@ function About() {
             <div>&nbsp;&nbsp;frameworks: ["React", "Three.js"],</div>
             <div>&nbsp;&nbsp;tools: ["Git", "Vite"],</div>
             <div>&#125;</div>
-            I like computers, clicky keyboards and creatings things that mean something
+            //I like computers, clicky keyboards and creatings things that mean something
           </div>
-        </div> */}
+        </div>
       </div>
     </div>
 
