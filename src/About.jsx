842ee1fb2ef643d322457d8b09a8f26cd0ad8364
n4ilvh <nailikespie@gmail.com>
2025-12-29 00:32:45 -0500

    update

--- src/About.jsx
+++ src/About.jsx
@@ -1,26 +1,28 @@
 import Image from "../media/selfie.jpg";
 import Barcode from "../media/barcode.webp";
+import { color } from "three/tsl";
 function About() {
   return (
     <div className="about"> 
    
       {/* <img src={Barcode} alt="description" className="about-barcode-img"/> */}
 
-      {/* <div className="about-title">About</div> */}
       <div className="about-container">
         <div className="about-image-container">
           <img src={Image} alt="description" className="about-img"/>
         </div>
         
           <div className="about-text">
-            <div>location: "Toronto, Canada"</div>
-            <div>born: 2006</div>
-            <div>school: "Carleton University"</div>
-            <div>interests: ["gaming", "music", "mechanical keyboards", "electric guitar", "basketball"]</div>
+            <div className="about-title">about.me</div>
+            <div>location: <span className="about-text-highlight">"Toronto, Canada"</span></div>
+            <div>born: <span className="about-text-highlight">2006</span></div>
+            <div>school: <span className="about-text-highlight">"Carleton University"</span></div>
+            <div>interests: [<span className="about-text-highlight">"gaming", "music", "mechanical keyboards", "electric guitar", "basketball"</span>]</div>
             <div>skills: &#123;</div>
-            <div>&nbsp;&nbsp;languages: ["Java", "Python", "C", "HTML, CSS"],</div>
-            <div>&nbsp;&nbsp;frameworks: ["React", "Three.js"],</div>
-            <div>&nbsp;&nbsp;tools: ["Git", "Vite"],</div>
+            <div>&nbsp;&nbsp;languages: [<span className="about-text-highlight">"Java", "Python", "C", "HTML, CSS"</span>],</div>
+            <div>&nbsp;&nbsp;frameworks: [<span className="about-text-highlight">"React", "Three.js"</span>],</div>
+            <div>&nbsp;&nbsp;tools: [<span className="about-text-highlight">"Git", "Vite" </span>],</div>
+            <div>&nbsp;&nbsp;applications: [<span className="about-text-highlight">"Adobe Photoshop", "Adobe Premiere Pro", "Adobe After Effects" </span>],</div>
             <div>&#125;</div>
             {/* //I like computers, clicky keyboards and creatings things that mean something */}
           
