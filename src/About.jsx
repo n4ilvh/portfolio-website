520aea920aee8dd4512148e51d1e6ec92a92fa0e
n4ilvh <nailikespie@gmail.com>
2026-01-02 01:23:40 -0500

    update

--- src/About.jsx
+++ src/About.jsx
@@ -46,20 +46,20 @@ function About() {
             <div>&nbsp;&nbsp;frameworks: [<span className="about-text-highlight">"React", "Three.js"</span>],</div>
             <div>&nbsp;&nbsp;tools: [<span className="about-text-highlight">"Git", "Vite" </span>],</div>
             <div className="interests-desktop">
-              <div>&nbsp;&nbsp;applications: [<span className="about-text-highlight">"Adobe Photoshop", "Adobe Premiere Pro", "Adobe After Effects" </span>],</div>
+              <div>&nbsp;&nbsp;applications: [<span className="about-text-highlight">"Adobe Photoshop", "Adobe Premiere Pro", "Adobe After Effects" </span>]</div>
             </div>
             <div className="interests-mobile">
-                <div>applications: [</div>
+                <div>&nbsp;&nbsp;applications: [</div>
                 <div className="about-indent">
-                  <span className="about-text-highlight">&nbsp;&nbsp;"Adobe Photoshop",</span>
+                  <span className="about-text-highlight">&nbsp;&nbsp;&nbsp;&nbsp;"Adobe Photoshop",</span>
                 </div>
                 <div className="about-indent">
-                  <span className="about-text-highlight">&nbsp;&nbsp;"Adobe Premiere Pro",</span>
+                  <span className="about-text-highlight">&nbsp;&nbsp;&nbsp;&nbsp;"Adobe Premiere Pro",</span>
                 </div>
                 <div className="about-indent">
-                  <span className="about-text-highlight">&nbsp;&nbsp;"Adobe After Effects",</span>
+                  <span className="about-text-highlight">&nbsp;&nbsp;&nbsp;&nbsp;"Adobe After Effects"</span>
                 </div>
-                <div>]</div>
+                <div>&nbsp;&nbsp;]</div>
             </div>
             <div>&#125;</div>
             {/* //I like computers, clicky keyboards and creatings things that mean something */}
