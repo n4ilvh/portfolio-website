fe149c0514eff8bacd34cc3747a546d9a837e87c
n4ilvh <nailikespie@gmail.com>
2026-01-09 00:48:01 -0500

    fix about me styling

--- src/About.jsx
+++ src/About.jsx
@@ -1,10 +1,14 @@
 import Image from "../media/selfie.jpg";
 import Barcode from "../media/barcode.webp";
+import LeftBlob from "../media/left-blob.png"
+import RightBlob from "../media/right-blob.png"
 import { color } from "three/tsl";
 function About() {
   return (
     <div className="about"> 
       {/* <img src={Barcode} alt="description" className="about-barcode-img"/> */}
+      <img src={LeftBlob} alt="description" className="left-blob"/>
+      <img src={RightBlob} alt="description" className="left-blob"/>
       <div className="about-container">
         <div className="title-container">
           <div className="about-title">About Me</div>
@@ -15,7 +19,7 @@ function About() {
             <img src={Image} alt="description" className="about-img"/>
           </div>
           <div className="about-text">
-            <div>location: <span className="about-text-highlight">"Toronto, Canada"</span></div>
+            <div className="noise-shadow">location: <span className="about-text-highlight">"Toronto, Canada"</span></div>
             <div>born: <span className="about-text-highlight">2006</span></div>
             <div>school: <span className="about-text-highlight">"Carleton University"</span></div>
             <div className="interests-desktop">
@@ -26,16 +30,24 @@ function About() {
               ]
             </div>
             <div>skills: &#123;</div>
+
+            
             <div>&nbsp;&nbsp;languages: [<span className="about-text-highlight">"Java", "Python", "C", "HTML", "CSS"</span>],</div>
             <div>&nbsp;&nbsp;frameworks: [<span className="about-text-highlight">"React", "Three.js"</span>],</div>
             <div>&nbsp;&nbsp;tools: [<span className="about-text-highlight">"Git", "Vite" </span>],</div>
             <div>&nbsp;&nbsp;software: [<span className="about-text-highlight">"Adobe Photoshop", "Adobe Premiere Pro", "Adobe After Effects"</span>]</div>
+
+
+
+
+
             <div>&#125;</div>
             {/* //I like computers, clicky keyboards and creatings things that mean something */}
           </div>
         </div>
       </div>
     </div>
+    
     // skills 
   );
 }
