852ffb3b505c3f857c15da1f07c78023bf4a7115
n4ilvh <nailikespie@gmail.com>
2026-04-27 21:27:45 -0400

    fix branches

--- src/App.jsx
+++ src/App.jsx
@@ -29,7 +29,7 @@ function App() {
           <nav>
             <div className="nav-item-container">
               <div className="nav-item"><Link to="/">Home</Link></div>
-              <div className="nav-item"><Link to="/about">About</Link></div>
+              {/* <div className="nav-item"><Link to="/about">About</Link></div> */}
               <div className="nav-item"><Link to="/projects">Projects</Link></div>
               {/* <div className="nav-item"><Link to="/services">Services</Link></div> */}
             </div>
