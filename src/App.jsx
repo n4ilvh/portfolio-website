506e4f839d8ae7b5a96d927f21f093a9112ac3d9
n4ilvh <nailikespie@gmail.com>
2026-06-27 14:55:41 -0400

    change up home design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { VscChromeRestore, VscChromeMinimize, VscChromeClose } from "react-icons/vsc";
+
 import Dither from "./Dither";
 import "./App.css";
 import About from "./About";
@@ -17,6 +19,7 @@ function App() {
             <div className="nav-item"><a href="#experience">EXPERIENCE</a></div>
             <div className="nav-item"><a href="#skills">SKILLS</a></div>
             <div className="nav-item"><a href="#contact">CONTACT</a></div>
+            
           </div>
         </nav>
         {/* HOME PAGE */}
@@ -33,23 +36,46 @@ function App() {
               waveSpeed={0.09}
             />
           </div>
+          
           <div className="content">
             <div className="page-container">
+              
                 <div className="header">
                   <div>Nailah Abel</div>
                   <p>Computer Science Student</p>
                 </div>
                 <div className="contact-container">
                   <div className="contact-links">
-                      <a href="https://github.com/n4ilvh" target="_blank" rel="noopener noreferrer">github</a>
+                      {/* <a href="https://github.com/n4ilvh" target="_blank" rel="noopener noreferrer">github</a>
                       <a href="https://www.linkedin.com/in/nailah-abel/" target="_blank" rel="noopener noreferrer">linkedin</a>
                       <a href="mailto:nailah.abel@gmail.com" target="_blank" rel="noopener noreferrer">email</a>
+                      <a href="https://monkeytype.com/profile/n4ilvh" target="_blank" rel="noopener noreferrer">monkeytype</a> */}
                       <a href="https://nailahabel.dev/resume.pdf" target="_blank">resume</a>
-                      <a href="https://monkeytype.com/profile/n4ilvh" target="_blank" rel="noopener noreferrer">monkeytype</a>
-                      view projects
-                      resume
+                      <a href="#projects">view projects</a>
+                      
                   </div>
                 </div> 
+            <div className="background-blocks">
+            <img src="../media/blocks.png"></img> 
+          </div>
+
+                <div class="pixel-window">
+                  <div class="pixel-window-header">
+                      <span>hobbies.exe</span>
+                      <div class="window-controls">
+                        <VscChromeMinimize />
+                        <VscChromeRestore /> 
+                        <VscChromeClose />
+                      </div>
+                  </div>
+                  <div>
+                    <p></p>coding
+                    <p></p>music
+                    <p></p>basketball
+                    <p></p>video games
+                  </div>
+                </div>
+                
             </div>
           </div>
         </section>
@@ -58,8 +84,9 @@ function App() {
         {/* PROJECTS PAGE */}
         <section id="projects">
           <div className="content2">
+                        
+
           </div>
-              
         </section>
       </div> 
     </Router>
